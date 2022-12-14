1c9061b0a750343e4e2048d35fb104afb7451adf
Sarisha <88791199+Sarisha-T@users.noreply.github.com>
2022-12-14 15:49:38 +0530

    Update Home.jsx

--- FoodSoGood/client/src/pages/Home.jsx
+++ FoodSoGood/client/src/pages/Home.jsx
@@ -1,36 +1,40 @@
-import React,{useState,useEffect} from 'react'
-import {useDispatch,useSelector} from 'react-redux'//dispatch actions
-import foods from '../components/data.json'
-import Foodcards from '../components/Foodcards'
-import { getALLFoods } from '../actions/foodActions'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"; //dispatch actions
+import Foodcards from "../components/Foodcards";
+import { getALLFoods } from "../actions/foodActions";
 
 function Home() {
+  const dispatch = useDispatch();
+  const foodstate = useSelector((state) => state.getALLFoodsReducer);
 
- 
-  const dispatch=useDispatch()
-  const foodstate=useSelector((state)=>state.getALLFoodsReducer)
+  const { foods, err, loading } = foodstate;
 
-  const {food,err,load}=foodstate; 
-  
-  //object 
-useEffect(()=>{
-  dispatch(getALLFoods())
-},[])
+  //object
+  useEffect(() => {
+    dispatch(getALLFoods());
+  }, []);
   return (
     // loading foods to homepage
     <div>
-        <div className="row mx-5 my-0 ">
-          {load?(<h1>Load</h1>):err?(<h1>Something wrong</h1>):(
-            foods.map(food=>{
-              return <div className="col-md-3 py-0 text-center ">
-                  <Foodcards food={food}/>
+      <div className="row mx-5 my-0 ">
+        {loading ? (
+          <h1>Load</h1>
+        ) : err ? (
+          <h1>Something wrong</h1>
+        ) : (
+          foods.map((food) => {
+            return (
+              <div className="col-md-3 py-0 text-center" key={food._id}>
+                <div>
+                  <Foodcards food={food} />
+                </div>
               </div>
+            );
           })
-          )}
-        </div>
+        )}
+      </div>
     </div>
-  )
-
+  );
 }
 
-export default Home
+export default Home;
