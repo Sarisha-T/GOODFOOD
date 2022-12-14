f0ec49274e305aa04a5260c03bb0d01ae2ebd14c
Sarisha <88791199+Sarisha-T@users.noreply.github.com>
2022-12-14 16:04:09 +0530

    Update Payment.jsx

--- FoodSoGood/client/src/components/Payment.jsx
+++ FoodSoGood/client/src/components/Payment.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 import { useDispatch } from "react-redux";
 import { placeOrder } from "../actions/orderActions";
-export default function Payment(totalamt) {
+export default function Payment({totalamt}) {
   const dispatch = useDispatch();
 
   function generateToken(token) {
@@ -12,13 +12,11 @@ export default function Payment(totalamt) {
   return (
     <div>
       <StripeCheckout
-        amount={totalamt * 100}
+        amount={totalamt * 100} //amount will be converted to cents so convert to 100
         shippingAddress
         token={generateToken}
         currency="INR"
-        stripeKey={
-          "pk_test_51LqEUOSElQrpNwqzIMSByxqlUlJdWazKiEKAfJTfaZroUPOxz4O55tzBsfwJW3tZnRkrL42mxmUsj0o1PGR4LrGS00yysgdWKr"
-        }
+        stripeKey="pk_test_51LqEUOSElQrpNwqzIMSByxqlUlJdWazKiEKAfJTfaZroUPOxz4O55tzBsfwJW3tZnRkrL42mxmUsj0o1PGR4LrGS00yysgdWKr"
       >
         <button className="add my-2 py-2 justify-content-center">
           PAY NOW
