a7d24cd42a10b9b9964eef273a6ceecb7a9b9c23
Sarisha <88791199+Sarisha-T@users.noreply.github.com>
2022-12-14 15:56:05 +0530

    Update foodModel.js

--- FoodSoGood/server/model/foodModel.js
+++ FoodSoGood/server/model/foodModel.js
@@ -1,22 +1,22 @@
 const mongoose=require('mongoose')
 
 const foodSchema=new mongoose.Schema({
-    name: { type: String, required: true },
-    quantity: { type: Number, required: true },
-    category:{type: String, required: true},
-    prices: { type: Number, required: true },
-    description: { type: String, required: true, minlength: 20 },
-    image: { type: String, required: true },
-    restaurant: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'restaurants',
-    },
+    name: {type: String, require},
+    quantity: {type: Number, require},
+    category:{type: String, require},
+    prices: { type: Number, require},
+    description: { type: String, require, minlength: 20},
+    image: { type: String, require},
+    // restaurant: {
+    // type: mongoose.Schema.Types.ObjectId,
+    // required: true,
+    // ref: 'restaurants',
+    // },
 },
 {
     timestamps:true,
 })
 
-const foodModel=new mongoose.model("foods",foodSchema)
+const foodModel= mongoose.model("foods",foodSchema)
 
 module.exports=foodModel;
