21ce5cd6a10d6f97754f9f9cc5060bd24fd34b7f
Sarisha <88791199+Sarisha-T@users.noreply.github.com>
2022-12-14 15:54:48 +0530

    Update userModel.js

--- FoodSoGood/server/model/userModel.js
+++ FoodSoGood/server/model/userModel.js
@@ -1,17 +1,17 @@
 const mongoose=require('mongoose')
 
 const userSchema=new mongoose.Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true,unique:true},
-    mobilenum:{type:Number,maxlength:10},
-    password:{type:String,required:true,minlength:8},
+    name:{type:String, required:true},
+    email:{type:String, required:true,unique:true},
+    mobilenum:{type:Number, maxlength:10},
+    password:{type:String, minlength:8, required:true},
     role: { type: String, default: 'customer' },
-    isAdmin: { type: Boolean,required:true,default:false},
+    isAdmin: { type: Boolean, required:true, default:false},
 },
 {
     timestamps:true,
 })
 
-const userModel=new mongoose.model("user",userSchema)
+const userModel=mongoose.model("user",userSchema)
 
 module.exports=userModel;
